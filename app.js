c63c44f433e8606f862ba6835c0f6c87f0adf86d
kkc <kkc1909@gmail.com>
2019-02-03 22:05:35 +0530

    layout create

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var engine = require('ejs-locals');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
